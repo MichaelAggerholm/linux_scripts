821827d6c274f4b9d4c5a3b6ae94e2630077a614
Michael Aggerholm <mao@tigermedia.dk>
2023-04-05 08:19:20 +0200

    Dobbelt citat for at forhindre globbing og ordopdeling.

--- administration/users_resource_usage.sh
+++ administration/users_resource_usage.sh
@@ -6,7 +6,7 @@ USERS=$(who | awk '{print $1}')
 # Skriv ressourceforbrug for hver bruger
 for USER in $USERS; do
     echo "User: $USER"
-    top -b -n 1 -u $USER | awk 'NR<=7 {print}'
+    top -b -n 1 -u "$USER" | awk 'NR<=7 {print}'
     echo ""
 done
 
